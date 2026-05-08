5541b29a601d4cba4eeba93e2cc63ab4840e271b
chivehao <chivehao@ikaros.run>
2026-05-09 04:35:17 +0800

    add spring ApplicationContext

---
+++ spring/src/main/java/run/ikaros/mvp/spring/ApplicationContext.java
@@ -0,0 +1,4 @@
+package run.ikaros.mvp.spring;
+
+public class ApplicationContext {
+}
---
+++ spring/src/test/java/run/ikaros/mvp/spring/ApplicationContextTest.java
@@ -0,0 +1,7 @@
+package run.ikaros.mvp.spring;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationContextTest {
+
+}
